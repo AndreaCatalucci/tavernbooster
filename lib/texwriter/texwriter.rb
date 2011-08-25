bdcdb38f38c910016734e3c49b902a88a7bcc450
Andrea <a.catalucci@gmail.com>
2011-08-25 16:46:38 +0200

    auxfiles werent put in the right dir

--- lib/texwriter/texwriter.rb
+++ lib/texwriter/texwriter.rb
@@ -4,7 +4,7 @@ class InvoiceProducer
   attr_accessor :invoice_data
   
   DATA_FOLDER = "#{Rails.root}/lib/texwriter"
-  
+   
   def initialize
     template = File.read("#{DATA_FOLDER}/templates/invoice.tex.erb")
     @invoice = ERB.new template
@@ -24,8 +24,7 @@ class InvoiceProducer
     output_file_name = @file_name
     output_dir = "#{Rails.root}/public/invoices"
     aux_dir = "#{DATA_FOLDER}/auxfiles"
-    command = "pdflatex #{@input_file_name} -output-directory=#{output_dir} -job-name=#{output_file_name}
-      -aux-directory=#{@aux_dir}"
+    command = "pdflatex #{@input_file_name} -output-directory=#{output_dir} -aux-directory=#{aux_dir} -job-name=#{output_file_name}"
     system(command)
   end
 end
