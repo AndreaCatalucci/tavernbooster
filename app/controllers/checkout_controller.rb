967b2567a4d21e2a6df73f99620020f3d1c120d9
Andrea <a.catalucci@gmail.com>
2011-09-01 21:42:47 +0200

    error in paid total calculation

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,7 @@
 class CheckoutController < ApplicationController
   def index
     @paid_orders = Order.paid.order('created_at').all
-    @paid_price_sum = Order.unpaid.all.inject(0) {|acc, o| acc += o.total_price}
+    @paid_price_sum = Order.paid.all.inject(0) {|acc, o| acc += o.total_price}
     @paid_people_sum = Order.paid.sum(:people)
     @unpaid_orders = Order.unpaid.order('created_at DESC').all
     @unpaid_price_sum = Order.unpaid.all.inject(0) {|acc, o| acc += o.total_price}
