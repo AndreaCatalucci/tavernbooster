0573def06a94a7db475c41c28eab5943c82751e5
Andrea <a.catalucci@gmail.com>
2011-09-01 23:09:44 +0200

    no quotes on update

--- app/models/course_order.rb
+++ app/models/course_order.rb
@@ -10,8 +10,8 @@ class CourseOrder < ActiveRecord::Base
 
   def increase_number_cooked_by(byval)
     new_val = number_cooked + byval
-    sql = "UPDATE 'course_orders' SET 'number_cooked' = #{new_val} WHERE 'course_orders'.'course_id' =
-      #{course_id} AND 'course_orders'.'order_id' = #{order_id}"
+    sql = "UPDATE course_orders SET number_cooked = #{new_val} WHERE course_orders.course_id =
+      #{course_id} AND course_orders.order_id = #{order_id}"
     connection.update(sql)
   end
 end
